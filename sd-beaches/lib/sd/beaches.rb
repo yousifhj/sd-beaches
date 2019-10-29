3068fbbfa1dbc134e9c5dd74259be21776ea5f97
Huda Yousif <huda@thinkful.com>
2019-10-28 22:11:37 -0700

    updated code

---
+++ sd-beaches/lib/sd/beaches.rb
@@ -0,0 +1,8 @@
+require "sd/beaches/version"
+
+module Sd
+  module Beaches
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ sd-beaches/lib/sd/beaches/version.rb
@@ -0,0 +1,5 @@
+module Sd
+  module Beaches
+    VERSION = "0.1.0"
+  end
+end
